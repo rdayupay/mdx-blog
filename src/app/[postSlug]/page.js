4be49191351b24806ef8354979877483cc203e6d
rdayupay <raqueldayupay@gmail.com>
2024-04-06 02:13:41 +0800

    Use bright for code snippets

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -7,6 +7,7 @@ import { BLOG_TITLE } from '@/constants';
 import BlogHero from '@/components/BlogHero';
 
 import styles from './postSlug.module.css';
+import CodeSnippet from '@/components/CodeSnippet';
 
 export async function generateMetadata({ params }) {
   const { frontmatter } = await loadBlogPost(params.postSlug);
@@ -27,7 +28,12 @@ async function BlogPost({ params }) {
         publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote
+          source={content}
+          components={{
+            pre: CodeSnippet,
+          }}
+        />
       </div>
     </article>
   );
