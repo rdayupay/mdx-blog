45f903722614bc6add5f9472da98d58e906c293d
rdayupay <raqueldayupay@gmail.com>
2024-04-05 17:22:44 +0800

    Add metadata

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -2,10 +2,20 @@ import React from 'react';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
 import { loadBlogPost } from '@/helpers/file-helpers';
+import { BLOG_TITLE } from '@/constants';
 
 import BlogHero from '@/components/BlogHero';
 
 import styles from './postSlug.module.css';
+
+export async function generateMetadata({ params }) {
+  const { frontmatter } = await loadBlogPost(params.postSlug);
+
+  return {
+    title: `${frontmatter.title} • ${BLOG_TITLE}`,
+    description: frontmatter.abstract,
+  };
+}
 
 async function BlogPost({ params }) {
   const { frontmatter, content } = await loadBlogPost(params.postSlug);
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import {
-  Work_Sans,
-  Spline_Sans_Mono,
-} from 'next/font/google';
+import { Work_Sans, Spline_Sans_Mono } from 'next/font/google';
 import clsx from 'clsx';
 
-import { LIGHT_TOKENS, DARK_TOKENS } from '@/constants';
+import { LIGHT_TOKENS, DARK_TOKENS, BLOG_TITLE } from '@/constants';
 
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
@@ -23,6 +20,11 @@ const monoFont = Spline_Sans_Mono({
   weight: 'variable',
   variable: '--font-family-mono',
 });
+
+export const metadata = {
+  title: BLOG_TITLE,
+  description: 'abstract',
+};
 
 function RootLayout({ children }) {
   // TODO: Dynamic theme depending on user preference
