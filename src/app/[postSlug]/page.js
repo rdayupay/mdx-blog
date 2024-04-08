8f46fcf55a457a479cbd8936aad9e390c5573425
rdayupay <raqueldayupay@gmail.com>
2024-04-09 00:05:17 +0800

    Implement 404 error

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { notFound } from 'next/navigation';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
 import { loadBlogPost } from '@/helpers/file-helpers';
@@ -10,7 +11,16 @@ import BlogHero from '@/components/BlogHero';
 import styles from './postSlug.module.css';
 
 export async function generateMetadata({ params }) {
-  const { frontmatter } = await loadBlogPost(params.postSlug);
+  const blogPostData = await loadBlogPost(params.postSlug);
+
+  // If we can't locate the blog post, this will be a 404. This
+  // means that the returned value from this function won't
+  // actually be used. We'll return `null` purely to avoid an error.
+  if (!blogPostData) {
+    return null;
+  }
+
+  const { frontmatter } = blogPostData;
 
   return {
     title: `${frontmatter.title} • ${BLOG_TITLE}`,
@@ -19,7 +29,15 @@ export async function generateMetadata({ params }) {
 }
 
 async function BlogPost({ params }) {
-  const { frontmatter, content } = await loadBlogPost(params.postSlug);
+  const blogPostData = await loadBlogPost(params.postSlug);
+
+  // If there is no blog post with the slug taken from the route
+  // params, render a 404 page instead.
+  if (!blogPostData) {
+    notFound();
+  }
+
+  const { frontmatter, content } = blogPostData;
 
   return (
     <article className={styles.wrapper}>
